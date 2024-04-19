e3acee7f22f8e6872208ea047e6272a2d10bcf17
joe1606 <josephlamkl75@outlook.com>
2024-04-19 14:00:30 +0100

    to task 4 now

--- clown_api/app.py
+++ clown_api/app.py
@@ -5,7 +5,7 @@
 from flask import Flask, Response, request, jsonify
 from psycopg2.errors import ForeignKeyViolation
 
-from database import get_db_connection
+from database import get_db_connection, get_cursor
 
 app = Flask(__name__)
 conn = get_db_connection()
@@ -26,8 +26,9 @@ def get_clowns() -> Response:
     Creates a new clown in response to a POST request."""
     if request.method == "GET":
         with conn.cursor() as cur:
-            cur.execute("SELECT clown_id FROM clown;")
+            cur.execute("SELECT * FROM clown;")
             return jsonify(cur.fetchall())
+
     else:
         data = request.json
         try:
@@ -51,6 +52,59 @@ def get_clowns() -> Response:
             return jsonify({
                 "message": err.args[0]
             }), 400
+
+def find_clown_from_id(cursor, clown_id) -> bool:
+    """Returns true if the clown exists in the database"""
+    cursor.execute(
+        "SELECT clown_id FROM clown", (clown_id, ))
+    valid_id = cursor.fetchall()
+    return clown_id in [id['clown_id'] for id in valid_id]
+
+@app.route("/clown/<int:clown_id>", methods=["GET"])
+def get_clown(clown_id: int) -> Response:
+    """Returns clown information based on ID input"""
+
+    if request.method == "GET":
+
+        connection = get_db_connection()
+        cursor = get_cursor(connection)
+
+
+        if not find_clown_from_id(cursor, clown_id):
+            return jsonify({'error': 'matching ID not found for clown'}), 404
+
+        cursor.execute(
+            "SELECT * FROM clown WHERE clown_id = %s;", (clown_id, ))
+        return_data = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+        return jsonify(return_data)
+
+
+@app.route("/clown/<int:clown_id>/review", methods=["POST"])
+def review_clown(clown_id: int) -> Response:
+    """Returns clown information based on ID input"""
+    data = request.json
+    connection = get_db_connection()
+    cursor = get_cursor(connection)
+
+    if "rating" not in data:
+        return {'error': 'Need clown rating.'}, 400
+
+    rating = data['rating']
+    if rating < 1 or rating > 5:
+        return {'error': 'Rating must be from 1 to 5'}, 400
+
+    cursor.execute(
+        """INSERT INTO review(clown_id, rating)
+            VALUES (%s, %s);""", (clown_id, rating))
+
+    connection.commit()
+    cursor.close()
+    conn.close()
+
+    return {'success': 'rating for clown added'}, 200
 
 
 if __name__ == "__main__":
--- clown_api/database.py
+++ clown_api/database.py
@@ -1,10 +1,11 @@
 """This file contains methods for communicating with the database."""
 
 from psycopg2 import connect
+from psycopg2._psycopg import connection
 from psycopg2.extras import RealDictCursor
+import psycopg2.extras
 
-
-def get_db_connection():
+def get_db_connection() -> connection:
     """Returns a connection to the database; all rows are returned as dicts."""
     return connect(
         dbname="clown",
@@ -12,3 +13,8 @@ def get_db_connection():
         port=5432,
         cursor_factory=RealDictCursor
     )
+
+
+def get_cursor(conn: connection):
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    return cursor
